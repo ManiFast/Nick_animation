a338290f49acfd856860b4c8aa93b4c38f71ccc4
MαηiFαsт <62830326+ManiFast@users.noreply.github.com>
2022-12-25 15:31:42 +0500

    Create logo.cpp

---
+++ logo.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+void Stas(int r = 4, int lb = 9, int d = 7)
+{
+  SetConsoleTextAttribute(hStdOut, d);
+
+  cout << "*==============*\n";
+
+  cout << "***************\r";
+  Sleep(100);
+  cout << "$(9%424 $^ &*@^\r";
+  Sleep(100);
+  cout << "(32#!^^ 4H fe52\r";
+  Sleep(100);
+  cout << ">>/fgsR#3Q4g4GE\r";
+  Sleep(100);
+  cout << "$#@^@Sa he $#Hs\r";
+  Sleep(100);
+  cout << ">>/fgsR#Eerwgv2\r";
+  Sleep(100);
+  cout << "@#@(*24 rw GjE3\r";
+  Sleep(100);
+  cout << "Đŕđű?š3 #f Jfd@\r";
+  Sleep(100);
+  cout << ">>/fgsR#E$#q@g#\r";
+  Sleep(100);
+  cout << "|+3015_ +_ >gR%\r";
+  Sleep(100);
+  cout << ">>/fgsR#E$#q@g#\r";
+  Sleep(100);
+  cout << ">>/fgsR#Eerwgv2\r";
+  Sleep(100);
+  cout << "C#@(*24 rw GjE3\r";
+  cout << "C$#5643 $g 43#$\r";
+  Sleep(100);
+  cout << "Cr#$433 3# gE$#\r";
+  cout << "Cr/fgsR#E$#q@g#\r";
+  Sleep(100);
+  cout << "Cre&#2! Hd {/P]\r";
+  cout << "Cre$#8h $G tH23\r";
+  Sleep(100);
+  cout << "Crea bdf@$#//zd\r";
+  cout << "Crea $#@785 4{#\r";
+  Sleep(100);
+  cout << "Creatdf *( >m,v\r";
+  cout << "Creat#$ $h bju7\r";
+  Sleep(100);
+  cout << "CreateR#Eerwgv2\r";
+  cout << "Create- (= *&#$\r";
+  Sleep(100);
+  cout << "Created rw GjE3\r";
+  cout << "Created H4 hj;L\r";
+  Sleep(100);
+  cout << "Created bf Jfd@\r";
+  cout << "Created b. 4E[|\r";
+  Sleep(100);
+  cout << "Created by#q@g#\r";
+  cout << "Created by e367\r";
+  Sleep(100);
+  cout << "Created by SOP]\r";
+  cout << "Created by Sh)$\r";
+  Sleep(100);
+  cout << "Created by Stz0\r";
+  cout << "Created by St^4\r";
+  Sleep(100);
+  cout << "Created by Stav\r";
+  cout << "Created by Sta?\r";
+
+  Sleep(130);
+
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "C";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "reated by Stas\r";
+  Sleep(50);
+
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "C";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "r";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "eated by Stas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Cr";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "e";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "ated by Stas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Cre";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "a";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "ted by Stas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Crea";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "t";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "ed by Stas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Creat";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "e";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "d by Stas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Create";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "d";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << " by Stas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Created";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << " b";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "y Stas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Created b";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "y";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << " Stas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Created by";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << " ";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Stas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Created by ";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "S";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "tas\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Created by S";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "t";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "as\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Created by St";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "a";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "s\r";
+  Sleep(50);
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "Created by Sta";
+  SetConsoleTextAttribute(hStdOut, r);
+  cout << "s";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "\r";
+  Sleep(50);
+
+
+
+  cout << "Created by ";
+  SetConsoleTextAttribute(hStdOut, lb);
+  cout << "Stas";
+  SetConsoleTextAttribute(hStdOut, d);
+  cout << "\n*==============*\n\n";
+
+  Beep(200, 100);
+  Beep(300, 100);
+}
+
+int main()
+{
+  Stas();
+}
